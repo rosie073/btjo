5b9300ef8a037fd22a5e710ffadb9bc9dd52403d
mitchay <camentomitchiean@gmail.com>
2026-03-02 08:30:42 +0800

    update

--- my-auth-app/src/App.jsx
+++ my-auth-app/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import ForgotReset from "./pages/ForgotReset.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot" element={<ForgotReset />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/Dashboard"/>} />
       </Routes>
     </BrowserRouter>
   );
